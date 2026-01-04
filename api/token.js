be44978dc89033342625bbeb3288a3bb236e24b4
ctudel <christudela@u.boisestate.edu>
2026-01-04 13:20:51 -0700

    Update function to use proper syntax

--- api/token.js
+++ api/token.js
@@ -1,7 +1,17 @@
-export default async function handler(req, res) {
-  const token = process.env.API_TOKEN;
-  return new Response(JSON.stringify({ token }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+export default {
+  fetch(request) {
+    const token = process.env.API_TOKEN;
+
+    if (!token) {
+      return new Response(null, {
+        status: 404
+      });
+    }
+
+    return new Response(JSON.stringify({ token }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+  }
 }
